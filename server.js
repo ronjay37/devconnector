a20a6f9a6ab091ad88df240da600b47b13bf306c
ROWEL FAJARDO <you@example.com>
2020-05-02 09:25:54 +0800

    Initial commit

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const connectDB = require('./config/db')
 
 const app = express();
 
+
+
 connectDB()
+
+
+//init middleware
+app.use(express.json({ extended: false }))
 
 app.get('/', (req, res) => {
     res.send('API running')
